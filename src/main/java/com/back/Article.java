58b4318ddc800dab43a3f14010ea57fb32a0a47c
kku1403 <kku1403@gmail.com>
2025-09-15 22:54:58 +0900

    [feat] appendIn 구현(T12~t14)

--- src/main/java/com/back/Article.java
+++ src/main/java/com/back/Article.java
@@ -1,4 +1,17 @@
 package com.back;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
 public class Article {
+    private int id;
+    private String title;
+    private String body;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+    private boolean isBlind;
 }
--- src/main/java/com/back/simpleDb/Sql.java
+++ src/main/java/com/back/simpleDb/Sql.java
@@ -5,6 +5,8 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Sql {
 
@@ -17,10 +19,23 @@ public class Sql {
     }
 
     public Sql append(String sql, Object... args) {
-        querySb.append(sql).append(" ");
         if(args.length > 0) {
             this.args.addAll(Arrays.asList(args));
         }
+        querySb.append(sql).append(" ");
+        return this;
+    }
+
+    public Sql appendIn(String sql, Object... args) {
+        if(args.length > 0) {
+            this.args.addAll(Arrays.asList(args));
+            String placeholder = IntStream.range(0, args.length)
+                    .mapToObj(i -> "?")
+                    .collect(Collectors.joining(", "));
+
+            sql = sql.replace("?", placeholder);
+        }
+        querySb.append(sql).append(" ");
         return this;
     }
 
@@ -48,7 +63,7 @@ public class Sql {
         return rows.getFirst();
     }
 
-    private <T> T selectColumn(Class<T> type) {
+    private <T> T selectColumnSingle(Class<T> type) {
         Map<String, Object> row = selectRow();
         if(row == null || row.isEmpty()) return null;
 
@@ -66,19 +81,37 @@ public class Sql {
         return null;
     }
 
+    public <T> List<T> selectColumnList(Class<T> type) {
+        List<Map<String, Object>> rows = selectRows();
+        if(rows == null || rows.isEmpty()) return null;
+
+        List<T> columnList = new ArrayList<>();
+        for (Map<String, Object> row : rows) {
+            if (!row.isEmpty()) {
+                Object value = row.values().iterator().next();
+                columnList.add(type.cast(value));
+            }
+        }
+        return columnList;
+    }
+
+    public List<Long> selectLongs() {
+        return selectColumnList(Long.class);
+    }
+
     public LocalDateTime selectDatetime() {
-        return selectColumn(LocalDateTime.class);
+        return selectColumnSingle(LocalDateTime.class);
     }
 
     public Long selectLong() {
-        return selectColumn(Long.class);
+        return selectColumnSingle(Long.class);
     }
 
     public String selectString() {
-        return selectColumn(String.class);
+        return selectColumnSingle(String.class);
     }
 
     public Boolean selectBoolean() {
-        return selectColumn(Boolean.class);
+        return selectColumnSingle(Boolean.class);
     }
 }
